acb41024cd41316dd0d8623f26899bd573a7a4bd
denis-verkholantsev <dverholancev@mail.ru>
2023-12-20 15:06:09 +0300

    Add plot

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from mergesort import mergesort
 from generating import generate_dataset
 import time
+import matplotlib.pyplot as plt
+from math import log2
+
+
+def func1(n):
+    return 2*n * log2(2*n) / (n * log2(n))
 
 
 def test(start_size, max_size, start_range, stop_range):
@@ -26,7 +32,24 @@ def test(start_size, max_size, start_range, stop_range):
 
     for i in range(len(avg)-1):
         print(f"T({sizes[i+1]})/T({ sizes[i] }) = {avg[i+1] / avg[i]}")
+    
+    return sizes, avg
 
-test(start_size=2**9, max_size=2**21, start_range=-100, stop_range=100)
 
 
+n_vals, time_vals = test(start_size=2**9, max_size=2**21, start_range=-100, stop_range=100)
+
+actual_values = [time_vals[i+1] / time_vals[i] for i in range(len(time_vals)-1)]
+priori_values = [func1(el) for el in n_vals[:-1]]
+
+
+plt.figure(figsize=(10, 6))
+plt.plot(n_vals[:-1], priori_values, color="red", label='2n*log2(2n)/(n*log2(n))')
+plt.plot(n_vals[:-1], actual_values, color="blue", label='actual')
+plt.ylim(0, 3)
+plt.xlabel('log(n)')
+plt.xscale('log') 
+plt.ylabel('func(n)')
+plt.legend()
+plt.grid(True)
+plt.savefig('plot1.png')
